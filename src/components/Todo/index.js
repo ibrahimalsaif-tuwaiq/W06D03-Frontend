b4f94796055c99156a858f13c9592401a9bd5b89
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-16 22:56:40 +0300

    app structure

---
+++ src/components/Todo/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './style.css'
+
+const Todo = () => {
+  return <div></div>;
+};
+
+export default Todo;
---
+++ src/components/Todos/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './style.css'
+
+const Todos = () => {
+  return <div></div>;
+};
+
+export default Todos;
